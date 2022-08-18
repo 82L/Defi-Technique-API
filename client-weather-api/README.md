bbf05de447cd771c1b0554a79e083cb7903e57c2
82L <16747176+82L@users.noreply.github.com>
2022-08-18 17:09:51 +0200

    [ADD] Initiate Client repo + connection to server socket

---
+++ client-weather-api/README.md
@@ -0,0 +1,21 @@
+# client-weather-api
+
+
+
+### Compile and Hot-Reload for Development
+
+```sh
+npm run dev
+```
+
+### Compile and Minify for Production
+
+```sh
+npm run build
+```
+
+### Lint with [ESLint](https://eslint.org/)
+
+```sh
+npm run lint
+```
---
+++ client-weather-api/src/App.vue
@@ -0,0 +1,31 @@
+<script>
+  import { useSocketIO } from "./service/socket.js";
+  const { socket } = useSocketIO()
+  socket.on("data", (apiData) =>{
+    console.log(JSON.parse(apiData));
+  })
+
+</script>
+<template>
+
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+}
+
+
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+
+
+
+}
+</style>
