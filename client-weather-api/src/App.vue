c5251e98781498a0635ffb25abd0b4103cb2513e
82L <16747176+82L@users.noreply.github.com>
2022-08-19 13:55:14 +0200

    [Client] Création de l'interface popup

--- client-weather-api/src/App.vue
+++ client-weather-api/src/App.vue
@@ -1,31 +1,28 @@
-<script>
-  import { useSocketIO } from "./service/socket.js";
-  const { socket } = useSocketIO()
-  socket.on("data", (apiData) =>{
-    console.log(JSON.parse(apiData));
-  })
+<script setup>
+
+import Popup from "./components/Popup.vue";
+import { ref } from 'vue';
+const pictoUrl = "http://openweathermap.org/img/wn/";
+const pictoEndUrl = "@2x.png";
+// Popup.methods.setPopup("true", "true", "true")
+// let self = this;
+const cityname = ref('City')
+const cast = ref('Loading')
+const picto = ref(pictoUrl + "01d" + pictoEndUrl)
+import { useSocketIO } from "./service/socket.js";
+const { socket } = useSocketIO();
+socket.on("data", (apiData) => {
+  let json = JSON.parse(apiData);
+  cityname.value = json.name;
+  picto.value= pictoUrl + json.weather[0].icon + pictoEndUrl;
+  cast.value=json.weather[0].description
+
+});
 
 </script>
 <template>
-
+  <Popup :city-name="cityname" :weather-cast="cast" :weather-picto="picto"/>
 </template>
-
 <style scoped>
-header {
-  line-height: 1.5;
-}
 
-
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-
-
-
-}
 </style>
---
+++ client-weather-api/src/components/Popup.vue
@@ -0,0 +1,69 @@
+<script setup>
+const props = defineProps(
+    {
+      cityName: {
+        type: String,
+        default: "Somewhere"
+      },
+      weatherPicto: {
+        type: String,
+        default: "Somewhere"
+      },
+      weatherCast: {
+        type: String,
+        default: "Somewhere"
+      }
+    }
+);
+</script>
+<template>
+  <div  ref="popup" class="popup">
+    <div class="header-container" >
+      <h3 class="city-name">{{ cityName }}</h3>
+      <img class="weather-picto" v-bind:src="weatherPicto" alt="">
+    </div>
+    <p class="weather-cast">{{ weatherCast }}</p>
+
+  </div>
+
+</template>
+<style scoped>
+
+
+.popup{
+  width: 438px;
+  border-radius: 42px;
+  position: absolute;
+  top: 40px;
+  left: 50%;
+  transform: translateX(-50%);
+  background-color:  #D9D9D9CC;
+  font-family: Inter, sans-serif;
+  padding-left: 30px;
+  padding-right: 20px;
+  padding-top: 10px;
+  padding-bottom: 20px;
+  max-width: 90%;
+}
+.city-name{
+  font-size: 32px;
+  font-weight: 700;
+}
+.weather-cast{
+  font-size: 18px;
+  font-weight: 400;
+}
+.weather-cast::first-letter{
+  text-transform: uppercase;
+}
+.weather-picto{
+  width: 40px;
+  height: 40px;
+}
+.header-container{
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+
+</style>
